7fa98b3014fe136b1a190881d10b477780aa58aa
AkaiAliev <alievakai@icloud.com>
2024-02-04 10:42:51 +0600

    Initial Commit

---
+++ MySparTestProject1221/MySparTestProject1221App.swift
@@ -0,0 +1,17 @@
+//
+//  MySparTestProject1221App.swift
+//  MySparTestProject1221
+//
+//  Created by Akai on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MySparTestProject1221App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
